d58dc66839409fa4c6127bfe9f8591512f2fcd56
Jarosław Bagnicki <jarek@bagnicki.net>
2019-02-06 23:38:24 +0100

    Second solution based on events

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var fs = require('fs');
+var EventEmmiter = require('events').EventEmitter;
 var colors = require('colors');
+
+var emmiter = new EventEmmiter();
 
 fs.readdir('./test-dir', function(err, files) {
   var filesStr = files.join('\n').concat('\n');
   console.log('Files read ...'.green);
-
-  fs.writeFile('./output.txt', filesStr, function(err) {
-    if (err) console.error(err);
-    console.log('Output written ...'.red);
-  })
+  emmiter.emit('files read', filesStr);
 })
+
+emmiter.on('files read', function(filesStr) {
+  fs.writeFile('./output.txt', filesStr, function(err) {
+  if (err) console.error(err);
+  console.log('Output written ...'.red);
+  })
+});
+
